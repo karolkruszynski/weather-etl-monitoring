7b63bd7ad7e32b6c482888539f3c1c3eb3fef517
karol <karol.kruszynski0@gmail.com>
2025-03-27 19:26:11 +0100

    error fix and added validation

--- src/python/extract_transform.py
+++ src/python/extract_transform.py
@@ -6,18 +6,24 @@ import pandas
 
 load_dotenv()   # Load variable from .env
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+# List of cities
 CITIES = ["Warsaw", "Berlin", "Paris"]
-API_URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
 def fetch_data():
     data = []
     for city in CITIES:
+        API_URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
         response = requests.get(API_URL.format(city=city))
+    if response.status_code == 200:
         data.append({
             "city": city,
             "temp": response.json()["main"]["temp"],
             "humidity": response.json()["main"]["humidity"]
         })
+    else:
+        print(f"Error for city {city}: {response.status_code}")
+
         df = pd.DataFrame(data)
         df.to_csv("weather_data.csv", index=False)
 
