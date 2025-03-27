37e5b0fc3d12733933fb81e8732a27ec7f225715
karol <karol.kruszynski0@gmail.com>
2025-03-27 18:38:50 +0100

    Create fetch data from API

--- src/python/extract_transform.py
+++ src/python/extract_transform.py
@@ -1,1 +1,22 @@
-git
+import pandas as pd
+import requests
+import pandas
+
+API_KEY =
+CITIES = ["Warsaw", "Berlin", "Paris"]
+API_URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+def fetch_data():
+    data = []
+    for city in CITIES:
+        response = requests.get(API_URL.format(city=city))
+        data.append({
+            "city": city,
+            "temp": response.json()["main"]["temp"],
+            "humidity": response.json()["main"]["humidity"]
+        })
+        df = pd.DataFrame(data)
+        df.to_csv("weather_data.csv", index=False)
+
+if __name__ == "__main__":
+    fetch_data()
